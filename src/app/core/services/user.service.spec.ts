cd54ea88eaaf09497ecd909ccd665c3b56c0ae4b
zura-japoshvili <japoshvili777@gmail.com>
2022-08-26 15:21:25 +0400

    NO PROGRESSION

--- src/app/core/services/user.service.spec.ts
+++ src/app/core/services/user.service.spec.ts
@@ -1,28 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
 import {HttpClientModule} from "@angular/common/http";
-import {RouterModule} from "@angular/router";
 import {RouterTestingModule} from "@angular/router/testing";
-import {userInfo} from "os";
 import {EmployeesForm} from "../Interfaces/employees-form";
 
 
 describe('UserService', () => {
   let service: UserService;
-  let expectedEmps: EmployeesForm[]
+  let expectedEms: EmployeesForm[]
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, RouterTestingModule]
     });
     service = TestBed.inject(UserService);
-    expectedEmps = [{}] as EmployeesForm[];
+    // expectedEms = [{}] as EmployeesForm[];
   });
 
   it('#getAllUsers should be created', () => {
     service.getAllUsers().subscribe(emp => {
-      expect(emp).toEqual(expectedEmps)
+      expect(emp).toEqual(expectedEms)
     })
 
   });
